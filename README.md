ff047026afc0f25634f2458e6c800850c6eaa5fb
maxl <max.leow@experian.com>
2019-07-24 19:51:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# testng-helical-example
+Example of code for learning purposes on following setup against helical-railgun (ver 0.1.3)
+- TestNg
---
+++ src/test/java/ComponentBTest.java
@@ -0,0 +1,12 @@
+import com.achelois.helical.annotations.CaseId;
+import com.achelois.helical.annotations.RunId;
+import org.testng.annotations.Test;
+
+@RunId(1)
+public class ComponentBTest {
+    @Test
+    @CaseId(11)
+    public void testComponentB(){
+
+    }
+}
